169455a71870f1ab3536af0b3359be8a4cf47eae
Karen <karen.nikoghosyan.her@gmail.com>
2024-07-12 18:09:28 +0400

    frontend is ready

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,37 @@
 import { ethers } from 'ethers'
+import { connect } from 'socket.io-client';
 
 const Navigation = ({ account, setAccount }) => {
 
+
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+
   return (
     <nav>
+      <div className='nav__brand'>
+        <h1>Dappcord</h1>
+      </div>
+      {account ? (
+        <button
+          type='button'
+          className='nav__connect'
+          onClick={connectHandler}
+        >
+          {account.slice(0, 7) + ' ... ' + account.slice(-5)}  
+        </button>
+      ) : (
+        <button
+          type='button'
+          className='nav__connect'
+          onClick={connectHandler}
+        >
+          Connect  
+        </button>
+      )}
 
     </nav>
   );
